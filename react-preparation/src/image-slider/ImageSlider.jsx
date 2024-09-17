9f97d6fdb00d0b05cf710e9ae287ebd35becff27
prathvitomar <108338096+prathvitomar@users.noreply.github.com>
2024-09-17 13:46:07 +0530

    Solved all bugs of Image Slider Components

--- react-preparation/src/image-slider/ImageSlider.jsx
+++ react-preparation/src/image-slider/ImageSlider.jsx
@@ -1,98 +1,98 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import './style.css';
 
+function ImageSlider({ url, limit = 5, page = 1 }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-// https://picsum.photos/v2/list
-function ImageSlider({
-    url,
-    page,
-    limit
-}) {
-
-    const [images, setImages] = useState([]);
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [errorMsg, setErrorMsg] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
-    async function fetchImages(getUrl) {
-      try {
-        setLoading(true);
-  
-        const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
-        const data = await response.json();
-  
-        if (data) {
-          setImages(data);
-          setLoading(false);
-        }
-      } catch (e) {
-        setErrorMsg(e.message);
+  const fetchData = async (urlVal, pageVal, limitVal) => {
+    try {
+      const response = await fetch(`${urlVal}?page=${pageVal}&limit=${limitVal}`);
+      const result = await response.json();
+      if (result) { 
+        setImages(result);
+        setError("");
         setLoading(false);
       }
+    } catch (error) {
+      setErrorMsg(error.message);
     }
+  };
+
+
+  useEffect(() => {
+    const debouncing = setTimeout(() => {
+      if (url) {
+        fetchData(url, page, limit);
+      }
+    }, 1000); // Debouncing with 1 second delay
+
+    // Cleanup function
+    return () => {
+      clearTimeout(debouncing);
+    };
+  }, [url, page, limit]);
   
-    function handlePrevious() {
-      setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
-    }
-  
-    function handleNext() {
-      setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
-    }
-  
-    useEffect(() => {
-      if (url !== "") fetchImages(url);
-    }, [url]);
-  
-    console.log(images);
-  
-    if (loading) {
-      return <div>Loading data ! Please wait</div>;
-    }
-  
-    if (errorMsg !== null) {
-      return <div>Error occured ! {errorMsg}</div>;
-    }
-  
-    return (
-      <div className="container">
-        <BsArrowLeftCircleFill
-          onClick={handlePrevious}
-          className="arrow arrow-left"
-        />
+
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  }
+
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+
+  if (loading) {
+    return <div>Loading data ! Please wait</div>;
+  }
+
+  return (
+    <>
+    <div className="container">
+      <BsArrowLeftCircleFill
+        onClick={handlePrevious}
+        className="arrow arrow-left"
+      />
+      {images && images.length
+        ? images.map((imageItem, index) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className={
+                currentSlide === index
+                  ? "current-image"
+                  : "current-image hide-current-image"
+              }
+            />
+          ))
+        : null}
+      <BsArrowRightCircleFill
+        onClick={handleNext}
+        className="arrow arrow-right"
+      />
+      <span className="circle-indicators">
         {images && images.length
-          ? images.map((imageItem, index) => (
-              <img
-                key={imageItem.id}
-                alt={imageItem.download_url}
-                src={imageItem.download_url}
+          ? images.map((_, index) => (
+              <button
+                key={index}
                 className={
                   currentSlide === index
-                    ? "current-image"
-                    : "current-image hide-current-image"
+                    ? "current-indicator"
+                    : "current-indicator inactive-indicator"
                 }
-              />
+                onClick={() => setCurrentSlide(index)}
+              ></button>
             ))
           : null}
-        <BsArrowRightCircleFill
-          onClick={handleNext}
-          className="arrow arrow-right"
-        />
-        <span className="circle-indicators">
-          {images && images.length
-            ? images.map((_, index) => (
-                <button
-                  key={index}
-                  className={
-                    currentSlide === index
-                      ? "current-indicator"
-                      : "current-indicator inactive-indicator"
-                  }
-                  onClick={() => setCurrentSlide(index)}
-                ></button>
-              ))
-            : null}
-        </span>
-      </div>
-    );
+      </span>
+    </div>
+    </>
+  )
+
 }
 
 export default ImageSlider
--- react-preparation/src/image-slider/MainImageSlider.jsx
+++ react-preparation/src/image-slider/MainImageSlider.jsx
@@ -5,9 +5,9 @@ function MainImageSlider() {
   return (
     <>
         <ImageSlider
-        url="https://picsum.photos/v2/list"
-        limit={10}
-        page={1}
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
         />
     </>
   )
